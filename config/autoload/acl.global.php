9bb5e0467e9e40e5c0e6e574a0aff0547322a2fd
Julián <juliangorge@hotmail.com>
2022-04-24 02:11:42 -0300

    Add ACL

---
+++ config/autoload/acl.global.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+	'acl' => [
+		'roles' => [
+			/*
+			Herencia:
+				Guest hereda de NULL
+				Administración hereda de Guest
+				Gerencia hereda de Administración
+				Todo lo que Administración haga, lo puede hacer Gerencia
+				* Atención: Administración no es lo mismo que "Admin"
+			*/
+			'guest' => NULL,
+			'sindicato' => NULL,
+			'medico' => NULL,
+			'administracion' => 'guest',
+			'gerencia' => 'administracion',
+			'admin' => 'gerencia',
+		],
+		'resources' => [
+			'allow' => [
+				'Admin\Controller\AffiliatesController' => [
+					'all' => ['administracion']
+				],
+				'Admin\Controller\AffiliatesFamilyController' => [
+					'all' => ['gerencia']
+				],
+				'Admin\Controller\AffiliatesPrescriptionsController' => [
+					'all' => ['gerencia', 'medico']
+				],
+				'Admin\Controller\IndexController' => [
+					'all' => ['administracion']
+				],
+				'Admin\Controller\NewsController' => [
+					'all' => ['gerencia']
+				],
+				'Admin\Controller\UnionNewsController' => [
+					'all' => ['gerencia', 'sindicato']
+				],
+				'Admin\Controller\ProfessionalsController' => [
+					'all' => ['administracion']
+				],
+				'Admin\Controller\ClinicalHistoriesController' => [
+					'all' => ['gerencia', 'medico']
+				],
+				'Admin\Controller\VademecumController' => [
+					'all' => ['gerencia', 'medico']
+				],
+				'Admin\Controller\AffiliatesAuthorizationsController' => [
+					'all' => ['gerencia', 'medico']
+				],
+				'Admin\Controller\UsersController' => [
+					'all' => ['gerencia']
+				],
+			],
+		]
+	]
+];
