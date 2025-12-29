3854cf3d43211618744bc174e4b65efa40115b0f
juliangorge <juliangorge@hotmail.com>
2025-12-28 19:36:36 -0500

    refactor(relatives): update type_of_family_member_id to type_of_family_member for better clarity in data representation

--- module/Admin/src/Entity/Relatives.php
+++ module/Admin/src/Entity/Relatives.php
@@ -158,7 +158,7 @@ class Relatives
             'email' => ($this->email == NULL ? '' : $this->email),
             'birthday' => $this->birthday->format('d/m/Y'),
             'phone_number' => ($this->phone_number == NULL ? '' : $this->phone_number),
-            'type_of_family_member_id' => $this->type_of_family_member_id,
+            'type_of_family_member' => $this->type_of_family_member_id->getName(),
             'affiliate_dni' => $this->affiliate_dni,
             'credential_number' => $this->credential_number,
 
